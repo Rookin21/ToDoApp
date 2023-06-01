4ea0445c67b532c09e485481e2fae5a85ec16bad
Almas Arslanov <crazynick21@gmail.com>
2023-06-01 09:52:18 +0300

    Создание главного окна. Добавление переменных для дальнейшей реализации точного времени и списка дел.

---
+++ ToDoApp/Models/ToDoModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ToDoApp.Models
+{
+    public class ToDoModel
+    {
+        public DateTime _CreationDate = DateTime.Now;
+
+        private bool _IsDone;
+        private string _Text;
+
+        public bool IsDone { get { return _IsDone;} set { _IsDone = value; } }
+        public string Text { get { return _Text; } set { _Text = value; } }
+    }
+}
